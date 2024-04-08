c939a7a6a87ef5a36a1506d882a8fbbb9a282b54
Nicholas Lachhman <nickla@my.yorku.ca>
2024-04-07 23:16:14 -0400

    Updated server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,35 @@
 const express = require("express")
 const app = express();
 const cors = require("cors")
+const pool = require("./db")
 
 // Middleware
 app.use(cors())
-app.use(express.json());
+app.use(express.json()); //req.body
+
+//ROUTES//
+
+// Create todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *", 
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+})
+
+// Get all todos
+
+// Get a todo
+
+//Update a todo
+
+//Delete a todo
 
 // 5000 used by AirPlay on Mac
 app.listen(5001, () => {
