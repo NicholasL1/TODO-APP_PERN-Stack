76ca169db4b9e51123202515b6ab6d48064645e6
Nicholas Lachhman <nickla@my.yorku.ca>
2024-04-08 23:40:07 -0400

    RESTful API w Postgres DB created

--- server/index.js
+++ server/index.js
@@ -24,12 +24,48 @@ app.post("/todos", async (req, res) => {
 })
 
 // Get all todos
+app.get("/todos", async(req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo")
+    res.json(allTodos.rows)
+  } catch (error) {
+    console.log(error.message);
+  }
+})
 
 // Get a todo
+app.get("/todos/:id", async(req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
+})
 
 //Update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+    res.json("Todo was updated")
+  } catch (error) {
+    console.log(error.message);
+  }
+})
 
 //Delete a todo
+app.delete("/todos/:id", async(req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+    res.json("Todo was deleted");
+  } catch (err) {
+    console.log(err.message);
+  }
+})
 
 // 5000 used by AirPlay on Mac
 app.listen(5001, () => {
