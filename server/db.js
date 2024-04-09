447a61f398ad0ae4ce69fc1166a2f78b1d30e03b
Nicholas Lachhman <nickla@my.yorku.ca>
2024-04-09 00:20:04 -0400

    Added environment variables

--- server/db.js
+++ server/db.js
@@ -1,11 +1,16 @@
+// Path module is used to construct file paths
+const path = require('path');
+
+// Load dotenv configuration from the root directory
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "Speedy109901",
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
   host: "localhost",
   port: 5432,
-  database: "perntodo"
+  database: process.env.PG_DATABASE
 });
 
 module.exports = pool;
