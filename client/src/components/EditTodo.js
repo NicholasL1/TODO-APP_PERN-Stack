b6aa9b1ed14d0d00ce96db979bd83d46a50c026f
Nicholas Lachhman <nickla@my.yorku.ca>
2024-04-29 17:41:58 -0400

    Completed working frontend with API integration

---
+++ client/src/components/EditTodo.js
@@ -0,0 +1,88 @@
+import React, { Fragment, useState } from "react";
+
+export default function EditTodo({ todo }) {
+  const [description, setDescription] = useState(todo.description);
+
+  //edit the todo description
+  const updateDescription = async (e) => {
+    e.preventDefault(); //Prevents refresh upon method call
+    try {
+      const newDescription = { description };
+      const response = await fetch(
+        `http://localhost:5001/todos/${todo.todo_id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(newDescription),
+        }
+      );
+
+      window.location = "/"; // Refreshes the current page
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  return (
+    <Fragment>
+      <button
+        type="button"
+        class="btn btn-warning"
+        data-toggle="modal"
+        data-target={`#id${todo.todo_id}`}
+      >
+        Edit
+      </button>
+
+      <div
+        class="modal"
+        id={`id${todo.todo_id}`}
+        onClick={() => setDescription(todo.description)}
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Edit Todo</h4>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
+              >
+                &times;
+              </button>
+            </div>
+
+            <div class="modal-body">
+              <input
+                type="text"
+                className="form-control"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-warning"
+                data-dismiss="modal"
+                onClick={(e) => updateDescription(e)}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                class="btn btn-danger"
+                data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
