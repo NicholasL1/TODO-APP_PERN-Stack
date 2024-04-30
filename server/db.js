5679921fe109e6eed034f7d9a97e6eb3b54cf918
Nicholas Lachhman <nickla@my.yorku.ca>
2024-04-29 22:56:38 -0400

    Integrated AWS RDS connection

--- server/db.js
+++ server/db.js
@@ -1,16 +1,16 @@
 // Path module is used to construct file paths
-const path = require('path');
+const path = require("path");
 
 // Load dotenv configuration from the root directory
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: "localhost",
+  user: "postgres",
+  password: "PERNTODO109283",
+  host: "perntodo-database.c3s2mu6egsgs.us-east-2.rds.amazonaws.com",
   port: 5432,
-  database: process.env.PG_DATABASE
+  database: "perntodoDB",
 });
 
 module.exports = pool;
